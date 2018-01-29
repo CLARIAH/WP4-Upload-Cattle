35df8419570a4805cfa2daea643c21a33a21a3ae
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2018-01-29 15:21:30 +0100

    username account, create ds if not exists

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import Client from "@triply/client.js/build/src/App";
 
 const [, , token, username, datasetName,  ...files] = process.argv;
 async function run() {
-  const account = Client.get({ url: 'https://api.druid.datalegend.net', token: token }).getAccount();
+  const account = Client.get({ url: 'https://api.druid.datalegend.net', token: token }).getAccount(username);
 
 
   ///check whether account name exists
   await account.info();
 
   var dataset = account.getDataset(datasetName);
-  //check whether dataset name exists
-  await dataset.info();
+  try {
+    //check whether dataset name exists
+    await dataset.info();
+  } catch(e) {
+    //assuming it doesnt exist
+    dataset = await account.addDataset({accessLevel: 'public',name: datasetName})
+  }
 
 
   //Clear all linked data in this dataset
